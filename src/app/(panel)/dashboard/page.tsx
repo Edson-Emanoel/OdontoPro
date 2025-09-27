4ba253c5bfcc55175ae7d749ed367615b6b5bbf6
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-09-27 10:20:36 -0300

    feat: Bloqueei a rota de Dashboard, para quem não é logado, e adicionei o .env.example

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,9 +1,23 @@
-import { Header } from "@/app/(public)/_components/header";
+import getSession from "@/lib/getSession"
+import { redirect } from "next/navigation"
 
-export default function Dashboard(){
+export default async function Dashboard(){
+    const session = await getSession()
+
+
+    if(!session){
+        redirect("/")
+    }
+    
     return (
         <div>
-            Dash-o-board-o Da
+            <h1>Dash-o-board-o Da</h1>
+            
+            <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
+
+            <div className="w-full h-[600px] bg-gray-500 mb-10"></div>
+
+            <div className="w-full h-[600px] bg-gray-200 mb-10"></div>
         </div>
     )
 }
